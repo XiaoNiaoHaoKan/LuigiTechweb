a13882c2d8278870082b9eed838ebcda7b69f64c
Palla di pelo <yy@email.com>
2026-08-24 15:53:29 +0200

    Per merge

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -15,6 +15,27 @@
 
 // export { connectDB };
 
+// import "dotenv/config";
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     const uri =
+//       process.env.MONGO_URI ||
+//       "mongodb://site252614:Oejachu0@mongo_site252614:27017/site252614?authSource=admin";
+
+//     await mongoose.connect(uri, {
+//       serverSelectionTimeoutMS: 5000
+//     });
+
+//     console.log("MongoDB connected:", mongoose.connection.host);
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+//export { connectDB };
 import "dotenv/config";
 import mongoose from "mongoose";
 
@@ -22,7 +43,7 @@ const connectDB = async () => {
   try {
     const uri =
       process.env.MONGO_URI ||
-      "mongodb://site252614:Oejachu0@mongo_site252614:27017/site252614?authSource=admin";
+      "mongodb://127.0.0.1:27017/site252614";
 
     await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 5000
